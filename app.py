199b4d1aeae304e84c35b24e112657d096f9a6aa
skhapijulhossen <skhapijulhossen@gmail.com>
2024-08-23 19:00:25 +0530

    implemented base app. Still need to smoothen the streaming

--- app.py
+++ app.py
@@ -2,11 +2,14 @@ import streamlit as st
 import cv2
 import numpy as np
 import tempfile
+from PIL import Image
 import os
 import logging
-from PIL import Image
-
-
+import time
+import queue
+from streamlit.delta_generator import DeltaGenerator
+from typing import BinaryIO
+from src.motion_detection  import optical_flow_motion_detection, visualize_motion_vectors
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -31,57 +34,132 @@ st.markdown(
     """,
     unsafe_allow_html=True
 )
+
 st.divider()
+
 # Main Page Title and Caption
-st.title("Video Processing Frame by Frame")
-# Placeholder for caption
-st.caption("This model extracts to trial design, diseases, drugs, population,Heart_Disease, Hyperlipidemia, Diabetes, Age, Test, Test_Result, Birth_Entity, Drug_BrandName, Date, etc. relevant entities from clinical trial abstracts.")
-# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+st.title("Motion Tracker")
+st.caption("")
 
-import streamlit as st
-import cv2
-import tempfile
-import os
+def create_temp_video_file(video_file: BinaryIO, temp_dir: str = 'outputs/temp') -> str:
+    """
+    Create a temporary file to store a video.
 
-def process_frame(frame):
-    # Apply any processing to the frame (e.g., grayscale conversion)
-    # Example: Convert the frame to grayscale
-    processed_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    return processed_frame
+    Args:
+        video_file (BinaryIO): A file-like object containing the video data.
+        temp_dir (str, optional): The directory where the temporary file should be created. Defaults to 'outputs/temp'.
+
+    Returns:
+        str: The path to the created temporary file.
+    """
+    try:
+        os.makedirs(temp_dir, exist_ok=True)
+        with tempfile.NamedTemporaryFile(delete=False, dir=temp_dir, suffix=".mp4") as tfile:
+            tfile.write(video_file.read())
+            return tfile.name
+    except Exception as e:
+        logger.error(f"Error creating temporary video file: {e}")
+        return ""
+
+def process_frame(previous_frame: np.ndarray, curr_frame: np.ndarray):
+    previous_gray_frame = cv2.cvtColor(previous_frame, cv2.COLOR_RGB2GRAY)
+    curr_gray_frame = cv2.cvtColor(curr_frame, cv2.COLOR_RGB2GRAY)
+    motion_detected, motion_vectors, avg_magnitude, good_next_pts, good_prev_pts = optical_flow_motion_detection(prev_frame=previous_gray_frame, curr_frame=curr_gray_frame)
+    return motion_detected, motion_vectors, avg_magnitude, good_next_pts, good_prev_pts
+
+
+def stream_frames(frame, panel: DeltaGenerator):
+    panel.image(
+        image=frame,
+        caption="Preview",
+        width=500,
+        # use_column_width=True,
+        channels="BGR",
+    )
+
+
+
+def capture_frames(source: str):
+    cap = cv2.VideoCapture(source)
+    ret = True
+    ret, previous_frame = cap.read()
+    while ret:
+        ret, frame = cap.read()
+        if not ret:
+            break
+        time.sleep(0.1)
+        yield previous_frame, frame
+        previous_frame = frame
+
+    cap.release()
+
+
+
+
+def get_or_create_session_state_variable(key, default_value=None):
+    """
+    Retrieves the value of a variable from Streamlit's session state.
+    If the variable doesn't exist, it creates it with the provided default value.
+
+    Args:
+        key (str): The key of the variable in session state.
+        default_value (Any): The default value to assign if the variable doesn't exist.
+
+    Returns:
+        Any: The value of the session state variable.
+    """
+    if key not in st.session_state:
+        st.session_state[key] = default_value
+    return st.session_state[key]
+
 
 def main():
 
+    # configPanel, previewPanel
+    configPanel, previewPanel = st.columns([1, 3])
+    flag = False
+    source = None
 
-    # Upload video
-    video_file = st.file_uploader("Upload a video", type=["mp4", "avi", "mov"])
+    # Config Panel
+    with configPanel:
+        # Upload video
+        video_file = st.file_uploader("Upload a video", type=["mp4", "avi", "mov"])
 
-    if video_file is not None:
-        # Create a temporary file to store the video
-        tfile = tempfile.NamedTemporaryFile(delete=False, dir='outputs/temp')
-        tfile.write(video_file.read())
+        # button to start processing
+        if st.button("Start Processing"):
+            flag = True
 
-        # Open the video file
-        cap = cv2.VideoCapture(tfile.name)
+        if video_file is not None:
+            source = create_temp_video_file(video_file=video_file)
+            bufferQueue = queue.Queue(maxsize=500)
 
-        stframe = st.empty()  # Placeholder for displaying the video frames
+    # PreviewPanel
+    with previewPanel:
+        # two more columns named objectTrackingPanel, segmentationPanel
+        objectTrackingPanel, segmentationPanel = st.columns([1, 1])
 
-        while cap.isOpened():
-            ret, frame = cap.read()
-            if not ret:
-                break
+        with objectTrackingPanel:
+            if flag and source:
+                panel = st.empty()
 
-            # Process the frame
-            processed_frame = process_frame(frame)
+                for previous_frame, curr_frame in capture_frames(source):
+                    
+                    motion_detected, motion_vectors, avg_magnitude, good_next_pts, good_prev_pts = process_frame(previous_frame, curr_frame)
+                    if motion_detected:
+                        
+                        vframe = visualize_motion_vectors(prev_frame=previous_frame, curr_frame=curr_frame, prev_pts=good_prev_pts, next_pts=good_next_pts)
+                        stream_frames(vframe, panel)
+                        continue
+                    stream_frames(curr_frame, panel)
+                    previous_frame = curr_frame
+        
 
-            # Convert the processed frame to RGB (needed for Streamlit display)
-            processed_frame_rgb = cv2.cvtColor(processed_frame, cv2.COLOR_GRAY2RGB)
-
-            # Display the frame in Streamlit
-            stframe.image(processed_frame_rgb, channels="RGB")
-
-        # Release the video capture object and remove the temporary file
-        cap.release()
-        os.remove(tfile.name)
+        
+        
+        # Clean up the temporary file after processing
+        if source:
+            if os.path.exists(source):
+                os.remove(source)
 
 if __name__ == "__main__":
     main()
