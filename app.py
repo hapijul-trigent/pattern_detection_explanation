9f4eb634121465f11908e3cfebe97e0b71669beb
skhapijulhossen <skhapijulhossen@gmail.com>
2024-08-25 06:25:34 +0530

    state fixes required

--- app.py
+++ app.py
@@ -9,8 +9,17 @@ import time
 import queue
 from streamlit.delta_generator import DeltaGenerator
 from typing import BinaryIO
-from src.motion_detection  import optical_flow_motion_detection, visualize_motion_vectors
-from src.movement_tracker import annotate_frame_with_ssd
+from src.motion_detection  import visualize_motion_vectors
+from src.movement_tracker import run_yolo_tracker
+from src.loaders import load_yolov8
+from src.utils import (
+    get_or_create_session_state_variable, 
+    create_temp_video_file, 
+    stream_frames, 
+    capture_frames,
+    process_frame,
+    select_resolution
+)
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -42,91 +51,19 @@ st.divider()
 st.title("Motion Tracker")
 st.caption("")
 
-def create_temp_video_file(video_file: BinaryIO, temp_dir: str = 'outputs/temp') -> str:
+# To style the image further, you may need to use additional HTML/CSS in Streamlit:
+st.markdown(
     """
-    Create a temporary file to store a video.
-
-    Args:
-        video_file (BinaryIO): A file-like object containing the video data.
-        temp_dir (str, optional): The directory where the temporary file should be created. Defaults to 'outputs/temp'.
-
-    Returns:
-        str: The path to the created temporary file.
-    """
-    try:
-        os.makedirs(temp_dir, exist_ok=True)
-        with tempfile.NamedTemporaryFile(delete=False, dir=temp_dir, suffix=".mp4") as tfile:
-            tfile.write(video_file.read())
-            return tfile.name
-    except Exception as e:
-        logger.error(f"Error creating temporary video file: {e}")
-        return ""
-
-def process_frame(previous_frame: np.ndarray, curr_frame: np.ndarray):
-    previous_gray_frame = cv2.cvtColor(previous_frame, cv2.COLOR_RGB2GRAY)
-    curr_gray_frame = cv2.cvtColor(curr_frame, cv2.COLOR_RGB2GRAY)
-    motion_detected, motion_vectors, avg_magnitude, good_next_pts, good_prev_pts = optical_flow_motion_detection(prev_frame=previous_gray_frame, curr_frame=curr_gray_frame, threshold=1.5, min_distance=3, quality_level=0.3)
-    return motion_detected, motion_vectors, avg_magnitude, good_next_pts, good_prev_pts
-
-
-def stream_frames(frame, panel: DeltaGenerator):
-    panel.image(
-        image=frame,
-        caption="Preview",
-        width=500,
-        # use_column_width=True,
-        channels="BGR",
-    )
-
-
-
-def capture_frames(source: str):
-    cap = cv2.VideoCapture(source)
-    ret = True
-    ret, previous_frame = cap.read()
-
-    scaling_factor = 0.5
-    original_height, original_width = previous_frame.shape[:2]
-    new_width = int(original_width * scaling_factor)
-    new_height = int(original_height * scaling_factor)
-    previous_frame = cv2.GaussianBlur(
-        src=cv2.resize(previous_frame, (new_width, new_height), interpolation=cv2.INTER_AREA), 
-        ksize=(3,3), sigmaX=0
-    )
-    while ret:
-        ret, frame = cap.read()
-        if not ret:
-            break
-
-        frame = cv2.GaussianBlur(
-            src=cv2.resize(frame, (new_width, new_height), interpolation=cv2.INTER_AREA), 
-            ksize=(3,3), sigmaX=0
-        )
-
-        time.sleep(0.0001)
-        yield previous_frame, frame
-        previous_frame = frame
-
-    cap.release()
-
-
-
-
-def get_or_create_session_state_variable(key, default_value=None):
-    """
-    Retrieves the value of a variable from Streamlit's session state.
-    If the variable doesn't exist, it creates it with the provided default value.
-
-    Args:
-        key (str): The key of the variable in session state.
-        default_value (Any): The default value to assign if the variable doesn't exist.
-
-    Returns:
-        Any: The value of the session state variable.
-    """
-    if key not in st.session_state:
-        st.session_state[key] = default_value
-    return st.session_state[key]
+    <style>
+    data-testid[stImage] {
+        border: 5px solid #ff6347;
+        border-radius: 10px;
+        box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 
 def main():
@@ -140,9 +77,14 @@ def main():
     get_or_create_session_state_variable("good_next_pts", None)
     get_or_create_session_state_variable("good_prev_pts", None)
     get_or_create_session_state_variable("algortihm", default_value='Optical Flow')
+    get_or_create_session_state_variable("start_process_button_clicked", default_value=False)
+    get_or_create_session_state_variable("processed", default_value=False)
+
+    # Models
+    yolov8 = load_yolov8(model_path="models/yolov8n.pt")
 
     # configPanel, previewPanel
-    configPanel, previewPanel = st.columns([1, 3])
+    configPanel, previewPanel = st.columns([1, 3], gap="large")
     flag = False
     source = None
 
@@ -152,31 +94,43 @@ def main():
         video_file = st.file_uploader("Upload a video", type=["mp4", "avi", "mov"])
 
         # button to start processing
-        if st.button("Start Processing"):
-            flag = True
+        if not st.session_state.processed:
+            if not st.session_state.start_process_button_clicked:
+                st.button("Start Processing", on_click=lambda: st.session_state.update(start_process_button_clicked=True))
+            else:
+                processing, stop = st.columns([1.5, 3])
+                with processing:
+                    st.button("Processing...", disabled=True)
+                with stop:
+                    st.button("Stop", on_click=lambda: st.session_state.update(start_process_button_clicked=False))
+        else:
+            st.button("Processed", on_click=lambda: st.session_state.update(start_process_button_clicked=False))
+            
 
         if video_file is not None:
             source = create_temp_video_file(video_file=video_file)
 
         if video_file:
             st.header('Configure')
-            st.session_state.algortihm = st.selectbox(label='Algorithm', options=['Optical Flow', 'Segmentation', 'Single Shot Detectore(SSD)'])
+            st.session_state.algortihm = st.selectbox(label='Algorithm', options=['Optical Flow', 'YOLOv8'])
+            resolution_choice, resolution = select_resolution()
             
 
     # PreviewPanel
+    yolov8_thread = None
     with previewPanel:
         # two more columns named objectTrackingPanel, segmentationPanel
         objectTrackingPanel, segmentationPanel = st.columns([1, 1])
 
         with objectTrackingPanel:
-            if flag and source:
+            if st.session_state.start_process_button_clicked and source:
                 panel = st.empty()
-
-                for previous_frame, curr_frame in capture_frames(source):
-                    if st.session_state.algortihm == 'Optical Flow':
+                if st.session_state.algortihm == 'Optical Flow':
+                    for ret, previous_frame, curr_frame in capture_frames(source=source, resolution=resolution):
+                        
                         motion_detected, motion_vectors, magnitudes, good_next_pts, good_prev_pts = process_frame(previous_frame, curr_frame)
                         if motion_detected:
-                            
+                                
                             vframe = visualize_motion_vectors(
                                 prev_frame=previous_frame, curr_frame=curr_frame, 
                                 prev_pts=good_prev_pts, next_pts=good_next_pts, 
@@ -186,10 +140,14 @@ def main():
                             continue
                         stream_frames(curr_frame, panel)
                         previous_frame = curr_frame
-                    elif st.session_state.algortihm == 'Single Shot Detectore(SSD)':
-                        pass
-        
+                elif st.session_state.algortihm == 'YOLOv8':
+                    
+                    save_path = run_yolo_tracker(filename=source, model=yolov8, file_index=1, resolution=resolution)
+                    if save_path:
+                        panel.video(data=save_path, format='video/mp4', autoplay=True)
 
+                st.session_state.start_process_button_clicked = False
+                st.success('Sucessfully Processed!')
         
         
         # Clean up the temporary file after processing
