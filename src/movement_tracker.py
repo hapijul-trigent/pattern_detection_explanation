85e6135205fb5cd8d53ddae43c5f83c66ddcd5ba
skhapijulhossen <skhapijulhossen@gmail.com>
2024-08-25 06:25:12 +0530

    yolov8 movement tracker added

--- src/movement_tracker.py
+++ src/movement_tracker.py
@@ -1,39 +1,93 @@
 import cv2
 import numpy as np
+import streamlit as st
+import logging
+import threading
+from collections import defaultdict
 
-def annotate_frame_with_ssd(frame: np.ndarray) -> np.ndarray:
+
+
+### YOLOv8
+def run_yolo_tracker(filename, model, file_index, resolution) -> str:
     """
-    Annotates the input frame with bounding boxes and labels detected by SSD.
+    Runs a video file or webcam stream concurrently with the YOLOv8 model using threading.
+
+    Captures video frames from a given file or camera source and utilizes the YOLOv8 model for object
+    tracking. Runs in its own thread for concurrent processing.
 
     Args:
-        frame (np.ndarray): The input frame to be processed, expected to be a BGR image.
+        filename (str): The path to the video file or the identifier for the webcam/external camera source.
+        model (obj): The YOLOv8 model object.
+        file_index (int): An index to uniquely identify the file being processed, used for display purposes.
 
-    Returns:
-        np.ndarray: The annotated frame with bounding boxes and labels.
+    Note:
+        Press 'q' to quit the video display window.
     """
-    # Load the pre-trained SSD model and configuration files
-    net = cv2.dnn.readNetFromCaffe("models/deploy.prototxt", "models/mobilenet_iter_73000.caffemodel")
+    logger = logging.getLogger(__name__)
+    try:
+        # Open the video file or stream
+        video = cv2.VideoCapture(filename)
+        if not video.isOpened():
+            raise ValueError(f"Error opening video file or stream: {filename}")
 
-    # Prepare the frame for SSD
-    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104, 117, 123))
-    net.setInput(blob)
-    detections = net.forward()
+        stream_panel = st.empty()
+        track_history = defaultdict(lambda: [])
 
-    # Get frame dimensions
-    (h, w) = frame.shape[:2]
+        fourcc = cv2.VideoWriter_fourcc(*'XVID')
+        save_path = f'outputs/processed/new.mp4'
+        video_writer = cv2.VideoWriter(save_path, fourcc, 24, resolution)
 
-    # Loop over the detections
-    for i in range(detections.shape[2]):
-        confidence = detections[0, 0, i, 2]
+        if not video_writer.isOpened():
+            print("Error: Could not open video file for writing.")
         
-        if confidence > 0.5:  # Confidence threshold
-            # Extract the bounding box coordinates
-            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-            (startX, startY, endX, endY) = box.astype("int")
+        while video.isOpened():
+            success, frame = video.read()
+            if not success:
+                logger.warning(f"Failed to read frame from {filename}.")
+                break
 
-            # Draw bounding box and label on the frame
-            label = f"Object {i}"
-            cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 2)
-            cv2.putText(frame, label, (startX, startY - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-            print(f"Detected: {label}")
-    return frame
+            try:
+                # frame = cv2.resize(frame, resolution)
+                print(frame.shape)
+                results = model.track(frame, persist=True)
+                if not results or not results[0].boxes:
+                    logger.warning("No tracking results found.")
+                    continue
+
+                boxes = results[0].boxes.xywh.cpu()
+                track_ids = results[0].boxes.id.int().cpu().tolist()
+                # annotated_frame = results[0].plot()
+                annotated_frame = frame
+
+                for box, track_id in zip(boxes, track_ids):
+                    x, y, w, h = box
+                    track = track_history[track_id]
+                    track.append((float(x), float(y)))
+                    if len(track) > 24:
+                        track.pop(0)
+                    points = np.hstack(track).astype(np.int32).reshape((-1, 1, 2))
+                    cv2.polylines(annotated_frame, [points], isClosed=False, color=(57, 255, 20), thickness=16)
+
+                    if points.shape[0] > 2:
+                        cv2.arrowedLine(annotated_frame, points[-2, 0], points[-1, 0], color=(0, 92, 255), thickness=24, line_type=cv2.LINE_AA, tipLength=0.3)
+                    
+                if isinstance(annotated_frame, np.ndarray):
+                    # Stream
+                    stream_panel.image(annotated_frame, caption=f"Preview_Stream_{file_index}", channels="BGR")
+                    video_writer.write(annotated_frame)
+
+                else:
+                    logger.error("Error converting frame to numpy array.")
+            except Exception as e:
+                logger.error(f"Error processing frame: {e}")
+
+        # Release video sources
+        video.release()
+        video_writer.release()
+        logger.info(f"Finished processing video: {filename}")
+        return filename
+    except Exception as e:
+        logger.error(f"An error occurred: {e}")
+        return None
+
+
