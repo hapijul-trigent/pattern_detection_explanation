d9f890738b6663748dff29feb58dffbcffce49a2
happysk24 <hapijul.h.sk@gmail.com>
2024-08-28 05:28:26 +0000

    added YoloWorld loader

--- src/loaders.py
+++ src/loaders.py
@@ -1,11 +1,11 @@
 import streamlit as st
-from ultralytics import YOLO
+from ultralytics import YOLO, YOLOWorld
 import logging
 from typing import Tuple
 
 
 @st.cache_resource(show_spinner=False)
-def load_yolo_model(model_path="models/yolov10x.pt") -> Tuple[YOLO, YOLO]:
+def load_yolo_model(model_name="yolov10n.pt") -> YOLO:
     """
     Load YOLO models and cache them to optimize loading times.
 
@@ -19,8 +19,7 @@ def load_yolo_model(model_path="models/yolov10x.pt") -> Tuple[YOLO, YOLO]:
     logger = logging.getLogger(__name__)
     try:
         # Load the YOLO models
-        modelDetect = YOLO(model=model_path + "yolov10n.pt", task="detect")
-        modelSegment = YOLO(model=model_path + "yolov8n-seg.pt", task="segment")
+        model = YOLO(model='models/' + model_name, task="detect")
     except FileNotFoundError as e:
         logger.error("Model file not found: %s", e)
         raise
@@ -31,7 +30,38 @@ def load_yolo_model(model_path="models/yolov10x.pt") -> Tuple[YOLO, YOLO]:
         logger.error("An unexpected error occurred: %s", e)
         raise
 
-    return modelDetect, modelSegment
+    return model
+
+
+@st.cache_resource(show_spinner=False)
+def load_yolo_world_model(model_name) -> YOLOWorld:
+    """
+    Load YOLO models and cache them to optimize loading times.
+
+    Returns:
+        Model: Model (YOLOv8n).
+
+    Raises:
+        FileNotFoundError: If the model files are not found.
+        RuntimeError: If there is an error loading the models.
+    """
+    logger = logging.getLogger(__name__)
+    try:
+        # Load the YOLO models
+        model = YOLOWorld(model='models/' + model_name)
+    except FileNotFoundError as e:
+        logger.error("Model file not found: %s", e)
+        raise
+    except RuntimeError as e:
+        logger.error("Error loading model: %s", e)
+        raise
+    except Exception as e:
+        logger.error("An unexpected error occurred: %s", e)
+        raise
+
+    return model
+
+
 
 # @st.cache_resource(show_spinner=False)
 # def load_yolo_model_onnx(model_path="models/yolov8n.onnx") -> Tuple[ort.InferenceSession, ort.InferenceSession]:
